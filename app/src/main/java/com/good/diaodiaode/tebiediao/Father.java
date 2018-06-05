bb03d527f25cdeff4cd77a03a6032c86246a39e1
liz <277967086@qq.com>
2018-06-06 00:17:08 +0800

    dddddd

---
+++ app/src/main/java/com/good/diaodiaode/tebiediao/Father.java
@@ -0,0 +1,16 @@
+package com.ymatou.live;
+
+public class Father {
+
+    public void text1() {
+    }
+
+    protected void text2() {
+    }
+
+    private void text3() {
+    }
+
+    void text4() {
+    }
+}
---
+++ app/src/main/java/com/good/diaodiaode/tebiediao/Son.java
@@ -0,0 +1,21 @@
+package com.ymatou.live;
+
+public class Son extends Father {
+    @Override
+    public void text1() {
+
+    }
+
+    @Override
+    public void text2() {
+
+    }
+
+    public void text3() {
+    }
+
+    @Override
+    public void text4() {
+
+    }
+}
